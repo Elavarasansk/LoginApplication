420b94033149ca7c180d94fc2a1ccda1060e6cf1
Elavarasansk <30699123+Elavarasansk@users.noreply.github.com>
2019-11-26 18:38:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,58 @@ app.use(bodyParser.json());
 app.use('/',authRouter);
 
 app.listen(5000);
+
+
+
+
+Header -- >
+		Left -->	
+			Logo
+		Right -->
+			Login Button
+Section -->
+		Image   	Content
+		Content 	Image
+Footer -->
+		Products
+		Contact
+		Solution
+		Company
+		Service & Support
+		
+	
+1 . Login Page Design
+
+2 . Section Design
+
+3 . Footer design
+
+4 . Header design
+
+5 . Component Motion 
+
+6 . Back to Top
+
+7 . Flash for login
+
+---Application---
+
+1 . All component CSS & Style change
+
+2 . Profile Navigation ( App Bar material ui ) 
+
+3 . Like and not like symbol change 
+
+
+
+** Final **
+
+All Component should be responsive 
+
+******
+
+
+
+
+
+
